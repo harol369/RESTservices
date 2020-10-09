1cf81a3ee8933414c541fece32df5a87305a38bf
Alexis <alexisml1234@gmail.com>
2020-10-09 12:47:24 -0500

    Cambios(Alexis)

--- rest_api/model_deploy/model_deploy/settings.py
+++ rest_api/model_deploy/model_deploy/settings.py
@@ -75,13 +75,23 @@ WSGI_APPLICATION = 'model_deploy.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME':'libreriaALML',
+        'USER': 'postgres',
+        'PASSWORD':'1234',
+        'HOST':'localhost',
+        'PORT':'5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
